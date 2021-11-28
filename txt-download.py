256bbcd5f1e722ef41fdf2f476933beb6ab8b7a1
dwd11511 <1737499156@qq.com>
2021-11-28 14:13:19 +0800

    转移下载目录

--- txt-download.py
+++ txt-download.py
@@ -1,6 +1,21 @@
-import os,sys
-with open(sys.path[0]+'\\downloadlist.txt', "r") as listtxt:
+import os, sys
+
+with open(sys.path[0] + "\\downloadlist.txt", "r") as listtxt:
     for i in listtxt.readlines():
-        os.system('python ' + sys.path[0] + '\\bili_Download.py -ao -a ' + i)
-        print('*'*20)
-    print('全下载完啦')
+        try:
+            os.path.exists(sys.path[0]+"./download")
+        except FileNotFoundError :
+            os.mkdir(sys.path[0]+"./download")
+        try:
+            os.system(
+                "python "
+                + sys.path[0]
+                + "\\bili_Download.py -ao -o "
+                + sys.path[0]
+                + "/download/ -a "
+                + i
+            )
+        except Exception as e:
+            print(e)
+        print("*" * 40)
+    print("全下载完啦")
