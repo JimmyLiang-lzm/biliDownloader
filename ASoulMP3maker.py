244d209460c6c73f7a446c8bbe78ea42998f5955
sti1l-0 <1737499156@qq.com>
2021-12-02 23:12:19 +0800

    错别字修正

--- ASoulMP3maker.py
+++ ASoulMP3maker.py
@@ -48,5 +48,5 @@ with open(sys.path[0] + r"/downloadlist.txt", "r") as listtxt:
         print('以下链接视频下载失败，共计',len(errorlist),'个')
         print(errorlist)
     if lowQlist:
-        print('一下链接的码率低于128k，但仍下载下来了')
+        print('以下链接的码率低于128k，但仍下载下来了')
         print(lowQlist)
